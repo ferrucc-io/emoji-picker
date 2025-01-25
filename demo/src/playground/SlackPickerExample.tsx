b2aef2ba1de07732e977ec9d6f8bdae3a9473ad4
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2025-01-25 22:34:35 +0100

    Update

--- demo/src/playground/SlackPickerExample.tsx
+++ demo/src/playground/SlackPickerExample.tsx
@@ -1,38 +1,25 @@
 import { EmojiPicker } from '@ferrucc-io/emoji-picker';
-import { EmojiPickerConfig } from './types';
 
-interface SlackPickerExampleProps {
-  config: EmojiPickerConfig & {
-    emojisPerRow: number;
-    emojiSize: number;
-    containerHeight: number;
-  };
-}
-
-export function SlackPickerExample({ config }: SlackPickerExampleProps) {
+export function SlackPickerExample() {
   return (
-    <div className="rounded-[8px] border border-zinc-300 dark:border-zinc-600 overflow-hidden shadow-sm font-['Lato'] pt-1">
+    <div className="rounded-[8px] border border-zinc-300 dark:border-zinc-600 overflow-hidden shadow-sm font-['Lato'] pt-1 max-w-[300px]">
       <EmojiPicker 
         className="w-[300px]"
-        emojisPerRow={config.emojisPerRow}
-        emojiSize={config.emojiSize}
+        emojisPerRow={9}
+        emojiSize={36}
       >
         <EmojiPicker.Header className="w-full">
           <EmojiPicker.Input 
             placeholder="Search all emoji" 
             className="h-[36px] bg-white dark:bg-zinc-800 border border-zinc-300 dark:border-zinc-600 w-full rounded-[8px] text-[15px] focus:shadow-[0_0_0_1px_#1d9bd1,0_0_0_6px_rgba(29,155,209,0.3)] focus:border-transparent focus:outline-none mb-1"
-            hideIcon={config.hideIcon || true}
-            autoFocus={config.autoFocus}
+            hideIcon
           />
         </EmojiPicker.Header>
         <EmojiPicker.Group>
-          <EmojiPicker.List 
-            containerHeight={config.containerHeight}
-            hideStickyHeader={config.hideStickyHeader}
-          />          
+          <EmojiPicker.List containerHeight={320} />          
         </EmojiPicker.Group>
         <EmojiPicker.Preview>
-          {({ previewedEmoji }: { previewedEmoji: string | null }) => (
+          {({ previewedEmoji }) => (
             <>
               {previewedEmoji ? 
                 <EmojiPicker.Content />
