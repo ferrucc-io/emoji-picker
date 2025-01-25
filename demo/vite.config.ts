307b4b0b64f0eaab85f1a997404d90fb7b23c9a7
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2025-01-25 23:17:28 +0100

    fix: update base URL for custom domain

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, PluginOption } from 'vite';
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/emoji-picker/',
+  plugins: [react(), tailwindcss()] as PluginOption[],
+  base: '/',
   build: {
     outDir: 'dist',
   },
