33246cdbccc1cefd6c3e6740e7ff16a9190ae776
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2025-01-26 15:58:57 +0100

    Fix scrolling behaviour on some browsers

--- packages/emoji-picker/src/EmojiPicker/EmojiPickerGroup.tsx
+++ packages/emoji-picker/src/EmojiPicker/EmojiPickerGroup.tsx
@@ -7,8 +7,8 @@ export interface EmojiPickerGroupProps {
 
 export function EmojiPickerGroup({ children }: EmojiPickerGroupProps) {
   return (
-    <div className="relative flex-1 overflow-hidden">
-      <div className="absolute inset-0 overflow-y-auto">
+    <div className="flex-1 overflow-hidden">
+      <div className="h-full overflow-y-auto overscroll-contain will-change-scroll">
         {children}
       </div>
     </div>
